d78734f40bb617b0243e7459dc20cf27a71f3bc3
ChemistryProfessor <ChemistryProfessor10110@gmail.com>
2023-08-12 01:18:43 -0700

    the last comit for this project

--- app.js
+++ app.js
@@ -6,8 +6,19 @@ menuLinksFhoneBtn.addEventListener("click" , () => {
     menuLinksFhoneBtn.classList.toggle("bg-white");
 });
 
+const itemMenuPhone = document.querySelectorAll(".itemMenuPhone");
 
+itemMenuPhone.forEach((item) => {
+    item.addEventListener("click" , () => {
+        menuLinksFhone.classList.toggle("hidden");
+        menuLinksFhoneBtn.classList.toggle("bg-white");
+    });
+});
 
+const darkModeBtn = document.querySelector(".darkModeBtn");
+
+darkModeBtn.addEventListener("click" , () => {
+    document.body.classList.toggle("dark")});
 
 
 
